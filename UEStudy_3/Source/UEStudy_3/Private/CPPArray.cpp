7bb4a8dc9af2c6824c9b151e6559e2b3eaf53850
sho <>
2025-03-21 00:16:06 +0900

    Arrayの操作

---
+++ UEStudy_3/Source/UEStudy_3/Private/CPPArray.cpp
@@ -0,0 +1,134 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPPArray.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+ACPPArray::ACPPArray()
+{
+	// Event Dispathcer[OnPrintHello]にCustom Event[PrintHello]をバインドする
+	OnPrintHello.AddDynamic(this, &ACPPArray::PrintHello);
+}
+
+int32 ACPPArray::Sum(int32 A, int32 B)
+{
+	return A + B;
+}
+
+// Called when the game starts or when spawned
+void ACPPArray::BeginPlay()
+{
+	SetupInput();
+
+	if (IsPrintHello)
+	{
+		// Hello World!を出力する処理
+		PrintHello();
+	}
+	else
+	{
+		// 計算結果を出力する処理
+		PressedActionPrintCalcResult();
+	}
+}
+
+void ACPPArray::PrintCalcResult(const ECPPCalcType Type, const int32 A, const int32 B, const float PrintDuration)
+{
+	switch (Type)
+	{
+		case ECPPCalcType::Add:
+		{
+			// Add(足し算)の処理
+			int32 ResultAdd = Sum(CalcVarA, CalcVarB);
+			FString StrResultAdd = FString::Printf(TEXT("%d"), ResultAdd);
+			UKismetSystemLibrary::PrintString(
+				this
+				, StrResultAdd
+				, true
+				, true
+				, FColor::Red
+				, Duration
+				, TEXT("None"));
+			break;
+		}
+		case ECPPCalcType::Subtract:
+		{
+			// Subtract(引き算)の処理
+			int32 ResultSubtract = CalcVarA - CalcVarB;
+			FString StrResultSubtract = FString::Printf(TEXT("%d"), ResultSubtract);
+			UKismetSystemLibrary::PrintString(
+				this
+				, StrResultSubtract
+				, true
+				, true
+				, FColor::Yellow
+				, Duration
+				, TEXT("None"));
+			break;
+		}
+		case ECPPCalcType::Multiply:
+		{
+			// Multiply(掛け算)の処理
+			int32 ResultMultiply = CalcVarA * CalcVarB;
+			FString StrResultMultiply = FString::Printf(TEXT("%d"), ResultMultiply);
+			UKismetSystemLibrary::PrintString(
+				this
+				, StrResultMultiply
+				, true
+				, true
+				, FColor::Green
+				, Duration
+				, TEXT("None"));
+			break;
+		}
+		case ECPPCalcType::Divide:
+		{
+			// Divide(割り算)の処理
+			float ResultDivide = (float)CalcVarA / (float)CalcVarB;
+			FString StrResultDivide = FString::Printf(TEXT("%f"), ResultDivide);
+			UKismetSystemLibrary::PrintString(
+				this
+				, StrResultDivide
+				, true
+				, true
+				, FColor::Blue
+				, Duration
+				, TEXT("None"));
+			break;
+		}
+	}
+}
+
+void ACPPArray::SetupInput()
+{
+	// 入力を有効にする
+	EnableInput(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+
+	// HキーのPressedとReleasedをバインドする
+	InputComponent->BindKey(EKeys::H, IE_Pressed, this, &ACPPArray::PressedH);
+
+	// ActionMappingsに設定したActionをバインドする
+	InputComponent->BindAction("ActionPrintCalcResult", IE_Pressed, this, &ACPPArray::PressedActionPrintCalcResult);
+}
+
+void ACPPArray::PressedH()
+{
+	// Event Dispathcer[OnPrintHello]をコールする
+	OnPrintHello.Broadcast();
+}
+
+void ACPPArray::PressedActionPrintCalcResult()
+{
+	// 計算結果を出力する処理
+	PrintCalcResult(CalcTypes[TypeIndex], CalcVarA, CalcVarB, Duration);
+	TypeIndex++;
+	TypeIndex = TypeIndex % CalcTypes.Num();
+}
+
+void ACPPArray::PrintHello()
+{
+	int32 randomIndex = FMath::RandRange(0, Messages.Num()-1);
+	// Hello World!を出力する処理
+	UKismetSystemLibrary::PrintString(this, Messages[randomIndex], true, true, TextColor, Duration, TEXT("None"));
+}
